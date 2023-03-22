5a4590ffdc9bc816544ec10c24a73189b95d61d3
lucasgalarce <galarcelucas@gmail.com>
2023-03-22 20:22:02 -0300

    db env variables

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,6 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "secret";
+export const DB_NAME = process.env.DB_NAME;
+export const DB_USERNAME = process.env.DB_USERNAME;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
--- src/db.ts
+++ src/db.ts
@@ -3,14 +3,15 @@ import { DataSource } from "typeorm";
 import { Course } from "./entities/Course";
 import { Student } from "./entities/Student";
 import { User } from "./entities/User";
+import { DB_NAME, DB_USERNAME, DB_PASSWORD } from "./config";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
   port: 5432,
-  username: "postgres",
-  password: "admin",
-  database: "school-db",
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
   synchronize: true,
   // logging: true,
   entities: [Student, Course, User],
