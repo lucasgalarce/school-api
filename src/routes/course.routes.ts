e96fbb99a7829b088f19e19ddfd08820648919df
lucasgalarce <galarcelucas@gmail.com>
2023-03-22 19:36:24 -0300

    Protect more routes

--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -5,6 +5,6 @@ export default (router) => {
   router.get("/courses", courseController.getCourses);
   router.get("/courses/:id", courseController.getCourseById);
   router.post("/courses", auth, courseController.createCourse);
-  router.put("/courses/:id", courseController.updateCourse);
-  router.delete("/courses/:id", courseController.deleteCourse);
+  router.put("/courses/:id", auth, courseController.updateCourse);
+  router.delete("/courses/:id", auth, courseController.deleteCourse);
 };
--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,10 +1,10 @@
-// import { auth } from "../middlewares/auth";
+import { auth } from "../middlewares/auth";
 import studentController from "../controllers/student.controller";
 
 export default (router) => {
   router.get("/students", studentController.getStudents);
   router.get("/students/:id", studentController.getStudentById);
-  router.post("/students", studentController.createStudent);
-  router.put("/students/:id", studentController.updateStudent);
-  router.delete("/students/:id", studentController.deleteStudent);
+  router.post("/students", auth, studentController.createStudent);
+  router.put("/students/:id", auth, studentController.updateStudent);
+  router.delete("/students/:id", auth, studentController.deleteStudent);
 };
