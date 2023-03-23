acc292699e49b3370bef3e8bdedbb23dd98f4c21
lucasgalarce <galarcelucas@gmail.com>
2023-03-23 10:27:10 -0300

    db uri

--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,4 @@ export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "secret";
 export const DB_NAME = process.env.DB_NAME;
 export const DB_USERNAME = process.env.DB_USERNAME;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DATABASE_URL = process.env.DATABASE_URL;
--- src/db.ts
+++ src/db.ts
@@ -3,15 +3,16 @@ import { DataSource } from "typeorm";
 import { Course } from "./entities/Course";
 import { Student } from "./entities/Student";
 import { User } from "./entities/User";
-import { DB_NAME, DB_USERNAME, DB_PASSWORD } from "./config";
+import { DB_NAME, DB_USERNAME, DB_PASSWORD, DATABASE_URL } from "./config";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
   port: 5432,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+  // username: DB_USERNAME,
+  // password: DB_PASSWORD,
+  // database: DB_NAME,
+  url: DATABASE_URL,
   synchronize: true,
   // logging: true,
   entities: [Student, Course, User],
